84dc378e912013e3179106f7b281f79521d4b79f
Igor <ihornekhaienko@gmail.com>
2022-11-01 19:23:33 +0200

    Add HomeController

--- HospitalWeb/HospitalWeb/Program.cs
+++ HospitalWeb/HospitalWeb/Program.cs
@@ -1,6 +1,26 @@
+using HospitalWeb.Data;
+using HospitalWeb.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
