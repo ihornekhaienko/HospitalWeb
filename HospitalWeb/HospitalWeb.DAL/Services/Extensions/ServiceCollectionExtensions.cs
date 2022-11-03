7d39e43c99db6fd56423a80d51042d30c3497c79
Igor <ihornekhaienko@gmail.com>
2022-11-03 15:32:18 +0200

    Add AddressService

---
+++ HospitalWeb/HospitalWeb.DAL/Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using HospitalWeb.DAL.Services.Implementations;
+using HospitalWeb.DAL.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HospitalWeb.DAL.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddAddressService(this IServiceCollection services)
+        {
+            services.AddScoped<IAddressService, AddressService>();
+        }
+    }
+}
---
+++ HospitalWeb/HospitalWeb.DAL/Services/Interfaces/IAddressService.cs
@@ -0,0 +1,13 @@
+﻿using HospitalWeb.DAL.Entities;
+
+namespace HospitalWeb.DAL.Services.Interfaces
+{
+    public interface IAddressService
+    {
+        IEnumerable<Address>? GetAll();
+        Address? Get(int id);
+        Task<Address?> Create(string? address);
+        Task Update(Address address);
+        Task Delete(Address address);
+    }
+}
