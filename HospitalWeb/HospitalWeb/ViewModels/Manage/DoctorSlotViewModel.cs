e4f36aed2bfcc553d6137b1fefb1626259ad61a5
Igor <ihornekhaienko@gmail.com>
2022-11-06 20:18:48 +0200

    Update DoctorSlotViewModel.cs

--- HospitalWeb/HospitalWeb/ViewModels/Manage/DoctorSlotViewModel.cs
+++ HospitalWeb/HospitalWeb/ViewModels/Manage/DoctorSlotViewModel.cs
@@ -1,10 +1,27 @@
-﻿namespace HospitalWeb.ViewModels.Manage
+﻿using HospitalWeb.Attributes.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace HospitalWeb.ViewModels.Manage
 {
     public class DoctorSlotViewModel
     {
-        public string? Doctor { get; set; }
-        public DayOfWeek DayOfWeek { get; set; }
-        public TimeSpan StartTime { get; set; }
-        public TimeSpan EndTime { get; set; }
+        public string? DoctorId { get; set; }
+
+        public string? DoctorFullName { get; set; }
+
+        public int? ScheduleId { get; set; }
+
+        [Required]
+        [Display(Name = "Select day")]
+        public string? DayOfWeek { get; set; }
+
+        [Required]
+        [TimeLessThan("EndTime", ErrorMessage = "The start time must lower than the end time")]
+        [Display(Name = "Select starting time")]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        [Display(Name = "Select ending time")]
+        public DateTime EndTime { get; set; }
     }
 }
