88b0ad60f763c3572bd30040445ee785cdd11e9f
Igor <ihornekhaienko@gmail.com>
2022-11-06 20:19:11 +0200

    Create TimeLessThanAttribute.cs

---
+++ HospitalWeb/HospitalWeb/Attributes/Validation/TimeLessThanAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalWeb.Attributes.Validation
+{
+    public class TimeLessThanAttribute : ValidationAttribute
+    {
+        private readonly string _comparisonProperty;
+
+        public TimeLessThanAttribute(string comparisonProperty)
+        {
+            _comparisonProperty = comparisonProperty;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            ErrorMessage = ErrorMessageString;
+            var currentValue = (DateTime)value;
+
+            var property = validationContext.ObjectType.GetProperty(_comparisonProperty);
+
+            if (property == null)
+                throw new ArgumentException("Property with this name not found");
+
+            var comparisonValue = (DateTime)property.GetValue(validationContext.ObjectInstance);
+
+            if (currentValue > comparisonValue)
+                return new ValidationResult(ErrorMessage);
+
+            return ValidationResult.Success;
+        }
+    }
+}
