a0b08d94b8a891bbd803181b415e778654771c84
Igor <ihornekhaienko@gmail.com>
2022-11-10 17:11:18 +0200

    Add ProfileViewModels

---
+++ HospitalWeb/HospitalWeb/ViewModels/Manage/PatientProfileViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalWeb.ViewModels.Manage
+{
+    public class PatientProfileViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Phone number")]
+        [RegularExpression("^\\+?[1-9][0-9]{7,14}$")]
+        public string Phone { get; set; }
+
+        public byte[] Image { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Locality")]
+        public string Locality { get; set; }
+    }
+}
