4c05aceae0f21ab3bb5a0749c0a2dcc0c746c4af
Igor <ihornekhaienko@gmail.com>
2022-11-10 14:50:51 +0200

    Add ViewModelBuilders

---
+++ HospitalWeb/HospitalWeb.Filters/Builders/Implementations/ViewModelBuilderDirector.cs
@@ -0,0 +1,15 @@
+﻿using HospitalWeb.Filters.Builders.Interfaces;
+
+namespace HospitalWeb.Filters.Builders.Implementations
+{
+    public class ViewModelBuilderDirector
+    {
+        public void MakeViewModel(IViewModelBuilder builder)
+        {
+            builder.BuildFilterModel();
+            builder.BuildSortModel();
+            builder.BuildEntityModel();
+            builder.BuildPageModel();
+        }
+    }
+}
