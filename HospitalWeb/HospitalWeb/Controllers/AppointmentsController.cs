918b40de5f04b173854d770c365feb0e69aee73b
Igor <ihornekhaienko@gmail.com>
2022-11-14 17:28:14 +0200

    Add Appointments/Details View

---
+++ HospitalWeb/HospitalWeb/Controllers/AppointmentsController.cs
@@ -0,0 +1,53 @@
+﻿using HospitalWeb.DAL.Services.Implementations;
+using HospitalWeb.Services.Interfaces;
+using HospitalWeb.ViewModels.Appointments;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalWeb.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        private readonly ILogger<AppointmentsController> _logger;
+        private readonly IWebHostEnvironment _environment;
+        private readonly UnitOfWork _uow;
+        private readonly IFileManager _fileManager;
+
+        public AppointmentsController(
+            ILogger<AppointmentsController> logger,
+            IWebHostEnvironment environment,
+            UnitOfWork uow,
+            IFileManager fileManager
+            )
+        {
+            _logger = logger;
+            _environment = environment;
+            _uow = uow;
+            _fileManager = fileManager;
+        }
+
+        [Authorize(Roles = "Doctor, Patient")]
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            ViewBag.Image = await _fileManager.GetBytes(Path.Combine(_environment.WebRootPath, "files/images/profile.jpg"));
+
+            var appointment = _uow.Appointments
+                .Get(a => a.AppointmentId == id);
+
+            var model = new AppointmentViewModel
+            {
+                AppointmentId = appointment.AppointmentId,
+                Diagnosis = appointment.Diagnosis.DiagnosisName,
+                Prescription = appointment.Prescription,
+                AppointmentDate = appointment.AppointmentDate,
+                DoctorId = appointment.Doctor.Id,
+                Doctor = appointment.Doctor.ToString(),
+                DoctorSpecialty = appointment.Doctor.Specialty.SpecialtyName,
+                Patient = appointment.Patient.ToString()
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ HospitalWeb/HospitalWeb/ViewModels/Appointments/AppointmentViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace HospitalWeb.ViewModels.Appointments
+{
+    public class AppointmentViewModel
+    {
+        public int AppointmentId { get; set; }
+
+        public string Diagnosis { get; set; }
+
+        public string Prescription { get; set; }
+
+        public DateTime AppointmentDate { get; set; }
+
+        public string DoctorId { get; set; }
+
+        public string Doctor { get; set; }
+
+        public string DoctorSpecialty { get; set; }
+
+        public string Patient { get; set; }
+    }
+}
