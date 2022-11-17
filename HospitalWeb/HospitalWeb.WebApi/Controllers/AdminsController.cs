81d36419fe8da4e73425155d463f8d0b76f873f9
Igor <ihornekhaienko@gmail.com>
2022-11-17 18:36:52 +0200

    Update AdminsController.cs

--- HospitalWeb/HospitalWeb.WebApi/Controllers/AdminsController.cs
+++ HospitalWeb/HospitalWeb.WebApi/Controllers/AdminsController.cs
@@ -1,7 +1,8 @@
 ﻿using HospitalWeb.DAL.Entities.Identity;
 using HospitalWeb.DAL.Services.Implementations;
-using Microsoft.AspNetCore.Http;
+using HospitalWeb.WebApi.Models.SortStates;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HospitalWeb.WebApi.Controllers
 {
@@ -21,9 +22,87 @@ namespace HospitalWeb.WebApi.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Admin>> Get()
+        public IEnumerable<Admin> Get(
+            string searchString,
+            AdminSortState sortOrder = AdminSortState.Id,
+            int pageSize = 10,
+            int pageNumber = 1)
         {
-            return await _uow.Admins.GetAllAsync();
+            int totalCount = 0;
+
+            Func<Admin, bool> filter = (a) =>
+            {
+                bool result = true;
+
+                if (!string.IsNullOrWhiteSpace(searchString))
+                {
+                    result = a.Name.Contains(searchString, StringComparison.OrdinalIgnoreCase) ||
+                    a.Surname.Contains(searchString, StringComparison.OrdinalIgnoreCase) ||
+                    a.Email.Contains(searchString, StringComparison.OrdinalIgnoreCase);
+                }
+
+                return result;
+            };
+
+            Func<IQueryable<Admin>, IOrderedQueryable<Admin>> orderBy = (admins) =>
+            {
+                totalCount = admins.Count();
+
+                switch (sortOrder)
+                {
+                    case AdminSortState.NameAsc:
+                        admins = admins.OrderBy(a => a.Name);
+                        break;
+                    case AdminSortState.NameDesc:
+                        admins = admins.OrderByDescending(a => a.Name);
+                        break;
+                    case AdminSortState.SurnameAsc:
+                        admins = admins.OrderBy(a => a.Surname);
+                        break;
+                    case AdminSortState.SurnameDesc:
+                        admins = admins.OrderByDescending(a => a.Surname);
+                        break;
+                    case AdminSortState.EmailAsc:
+                        admins = admins.OrderBy(a => a.Email);
+                        break;
+                    case AdminSortState.EmailDesc:
+                        admins = admins.OrderByDescending(a => a.Email);
+                        break;
+                    case AdminSortState.PhoneAsc:
+                        admins = admins.OrderBy(a => a.PhoneNumber);
+                        break;
+                    case AdminSortState.PhoneDesc:
+                        admins = admins.OrderByDescending(a => a.PhoneNumber);
+                        break;
+                    case AdminSortState.LevelAsc:
+                        admins = admins.OrderBy(a => a.IsSuperAdmin);
+                        break;
+                    case AdminSortState.LevelDesc:
+                        admins = admins.OrderByDescending(a => a.IsSuperAdmin);
+                        break;
+                    default:
+                        admins = admins.OrderBy(a => a.Id);
+                        break;
+                }
+
+                return (IOrderedQueryable<Admin>)admins;
+            };
+
+            var admins = _uow.Admins
+                .GetAll(filter: filter, orderBy: orderBy, first: pageSize, offset: (pageNumber - 1) * pageSize);
+
+            var metadata = new
+            {
+                TotalCount = totalCount,
+                Count = admins.Count(),
+                PageSize = pageSize,
+                PageNumber = pageNumber,
+                TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize)
+            };
+
+            Response.Headers.Add("Pagination", JsonConvert.SerializeObject(metadata));
+
+            return admins;
         }
 
         [HttpGet("{id}")]
