df6f04e31a889a948ec2c0cdac4df2b3389d688d
Igor <ihornekhaienko@gmail.com>
2022-11-17 15:21:32 +0200

    Add SchedulesController

---
+++ HospitalWeb/HospitalWeb.WebApi/Controllers/SchedulesController.cs
@@ -0,0 +1,75 @@
+﻿using HospitalWeb.DAL.Entities;
+using HospitalWeb.DAL.Services.Implementations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalWeb.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SchedulesController : ControllerBase
+    {
+        private readonly ILogger<SchedulesController> _logger;
+        private readonly UnitOfWork _uow;
+
+        public SchedulesController(
+            ILogger<SchedulesController> logger,
+            UnitOfWork uow)
+        {
+            _logger = logger;
+            _uow = uow;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Schedule>> Get()
+        {
+            return await _uow.Schedules.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Schedule>> Get(int id)
+        {
+            return await _uow.Schedules.GetAsync(s => s.ScheduleId == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Schedule>> Post(Schedule schedule)
+        {
+            if (schedule == null)
+            {
+                return BadRequest();
+            }
+
+            await _uow.Schedules.CreateAsync(schedule);
+
+            return Ok(schedule);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Schedule>> Put(Schedule schedule)
+        {
+            if (schedule == null)
+            {
+                return BadRequest();
+            }
+
+            await _uow.Schedules.UpdateAsync(schedule);
+
+            return Ok(schedule);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Schedule>> Delete(int id)
+        {
+            var schedule = await _uow.Schedules.GetAsync(s => s.ScheduleId == id);
+
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            await _uow.Schedules.DeleteAsync(schedule);
+
+            return Ok(schedule);
+        }
+    }
+}
