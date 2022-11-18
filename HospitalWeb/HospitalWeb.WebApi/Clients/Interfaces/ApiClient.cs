5501c64712004dc1598467b7505a3a01fd611dca
Igor <ihornekhaienko@gmail.com>
2022-11-18 15:13:09 +0200

    Add ApiClient base

---
+++ HospitalWeb/HospitalWeb.WebApi/Clients/Interfaces/ApiClient.cs
@@ -0,0 +1,29 @@
+﻿namespace HospitalWeb.WebApi.Clients.Interfaces
+{
+    public abstract class ApiClient<TType, TIdentifier> : IApiClient<TType, TIdentifier>
+    {
+        protected readonly HttpClient _client;
+        protected readonly IConfiguration _config;
+
+        public ApiClient(IConfiguration config)
+        {
+            _client = new HttpClient();
+            _config = config;
+            _client.BaseAddress = new Uri(_config["WebApi:Url"]);
+        }
+
+        public abstract HttpResponseMessage Get();
+
+        public abstract HttpResponseMessage Get(TIdentifier identifier);
+
+        public abstract TType Read(HttpResponseMessage response);
+
+        public abstract HttpResponseMessage Post(TType obj);
+
+        public abstract HttpResponseMessage Put(TType obj);
+
+        public abstract HttpResponseMessage Delete(TType obj);
+
+        public abstract HttpResponseMessage Delete(TIdentifier identifier);
+    }
+}
---
+++ HospitalWeb/HospitalWeb.WebApi/Clients/Interfaces/IApiClient.cs
@@ -0,0 +1,19 @@
+﻿namespace HospitalWeb.WebApi.Clients.Interfaces
+{
+    public interface IApiClient<TType, TIdentifier>
+    {
+        public HttpResponseMessage Get();
+
+        public HttpResponseMessage Get(TIdentifier identifier);
+
+        public TType Read(HttpResponseMessage response);
+
+        public HttpResponseMessage Post(TType obj);
+
+        public HttpResponseMessage Put(TType obj);
+
+        public HttpResponseMessage Delete(TType obj);
+
+        public HttpResponseMessage Delete(TIdentifier identifier);
+    }
+}
