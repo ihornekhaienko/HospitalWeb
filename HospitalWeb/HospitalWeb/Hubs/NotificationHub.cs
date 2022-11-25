fd1d54ca1e647e70ac0f9c841f0bef6e562d6fe1
Igor <ihornekhaienko@gmail.com>
2022-11-25 23:24:32 +0200

    Add NotificationHub

---
+++ HospitalWeb/HospitalWeb/Hubs/NotificationHub.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace HospitalWeb.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task NotifySignUp(string receiver, string topic, string message)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(receiver))
+                {
+                    throw new ArgumentNullException(nameof(receiver));
+                }
+
+                await Clients.User(receiver).SendAsync("NotifySignUp", topic, message);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                throw;
+            }
+        }
+
+        public async Task NotifyCancel(string receiver, string topic, string message)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(receiver))
+                {
+                    throw new ArgumentNullException(nameof(receiver));
+                }
+
+                await Clients.User(receiver).SendAsync("NotifyCancel", topic, message);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                throw;
+            }
+        }
+    }
+}
