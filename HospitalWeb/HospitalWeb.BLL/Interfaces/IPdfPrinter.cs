7004fae462d5efcb959289f4f54852222bf35808
Igor <ihornekhaienko@gmail.com>
2022-11-25 14:30:44 +0200

    Create IPdfPrinter.cs

---
+++ HospitalWeb/HospitalWeb.BLL/Interfaces/IPdfPrinter.cs
@@ -0,0 +1,9 @@
+﻿using HospitalWeb.DAL.Entities;
+
+namespace HospitalWeb.Services.Interfaces
+{
+    public interface IPdfPrinter
+    {
+        public void PrintAppointment(Appointment appointment, string filePath);
+    }
+}
