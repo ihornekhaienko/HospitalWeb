41d4798face15f3f6679ca79d8ec4fe7113441df
Igor <ihornekhaienko@gmail.com>
2022-11-25 23:24:22 +0200

    Update Config

--- HospitalWeb/HospitalWeb/Program.cs
+++ HospitalWeb/HospitalWeb/Program.cs
@@ -126,7 +126,7 @@ app.UseEndpoints(endpoints =>
 });
 
 app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
