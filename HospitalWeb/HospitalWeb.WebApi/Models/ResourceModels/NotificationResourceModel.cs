4e495a7558c1e6ea0782ab4b8e2e2acc28966391
Igor <ihornekhaienko@gmail.com>
2022-11-26 16:42:49 +0200

    Add NotificationResourceModel

---
+++ HospitalWeb/HospitalWeb.WebApi/Models/ResourceModels/NotificationResourceModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HospitalWeb.WebApi.Models.ResourceModels
+{
+    public class NotificationResourceModel
+    {
+        public int NotificationId { get; set; }
+
+        public string Topic { get; set; }
+
+        public string Message { get; set; }
+
+        public bool IsRead { get; set; }
+
+
+        public string AppUserId { get; set; }
+    }
+}
