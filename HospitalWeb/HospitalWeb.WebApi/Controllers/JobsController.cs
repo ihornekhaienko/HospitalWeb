442a950f6e60689777488a5957909a18b663491a
Igor <ihornekhaienko@gmail.com>
2022-12-02 14:11:32 +0200

    Add JobsController

---
+++ HospitalWeb/HospitalWeb.WebApi/Controllers/JobsController.cs
@@ -0,0 +1,25 @@
+﻿using Hangfire;
+using HospitalWeb.WebApi.Clients.Implementations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalWeb.WebApi.Controllers
+{
+    public class JobsController : ControllerBase
+    {
+        private readonly ApiUnitOfWork _api;
+
+        public JobsController(ApiUnitOfWork api)
+        {
+            _api = api;
+        }
+
+        [HttpPost]
+        [Route("updateStates")]
+        public IActionResult UpdateStates()
+        {
+            RecurringJob.AddOrUpdate(() => _api.Appointments.UpdateStates(null, null), Cron.Daily);
+
+            return Ok();
+        }
+    }
+}
