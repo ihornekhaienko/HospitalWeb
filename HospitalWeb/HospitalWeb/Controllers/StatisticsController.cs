a8e40c2e5f6c52119ce9cff677a2f4b27c11f79a
Igor <ihornekhaienko@gmail.com>
2022-12-11 21:35:24 +0200

    Add Attendance Statistics

--- HospitalWeb/HospitalWeb/Controllers/StatisticsController.cs
+++ HospitalWeb/HospitalWeb/Controllers/StatisticsController.cs
@@ -1,6 +1,10 @@
 ﻿using HospitalWeb.Filters.Builders.Implementations;
+using HospitalWeb.Filters.Models.DTO;
+using HospitalWeb.Filters.Models.ViewModels;
+using HospitalWeb.ViewModels.Statistics;
 using HospitalWeb.WebApi.Clients.Implementations;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HospitalWeb.Controllers
 {
@@ -17,15 +21,26 @@ namespace HospitalWeb.Controllers
 
         [HttpGet]
         public IActionResult Index(
-            DateTime? fromDate,
-            DateTime? toDate)
+            DateTime? fromDate = null,
+            DateTime? toDate = null)
         {
-            var builder = new AppointmentsViewModelBuilder(_api, state: 3, fromTime: fromDate, toTime: toDate, pageSize: int.MaxValue);
+            var builder = new AppointmentsViewModelBuilder(_api, state: 5, fromTime: fromDate, toTime: toDate, pageSize: int.MaxValue);
             var director = new ViewModelBuilderDirector();
             director.MakeViewModel(builder);
             var viewModel = builder.GetViewModel();
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Attendance([FromBody] List<AppointmentDTO> appointments)
+        {
+            var model = appointments
+                .GroupBy(a => a.AppointmentDate.Date)
+                .Select(g => new AttendanceStatViewModel { VisitDate = g.Key, VisitsCount = g.Count() })
+                .ToList();
+
+            return Json(model);
         }
     }
 }
