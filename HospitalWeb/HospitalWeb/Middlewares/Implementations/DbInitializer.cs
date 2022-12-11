6070f6833ebc1960a66b6a1bf70a0ff2c273401d
Igor <ihornekhaienko@gmail.com>
2022-12-11 15:52:59 +0200

    Update DbInitializer Middleware

--- HospitalWeb/HospitalWeb/Middlewares/Implementations/DbInitializer.cs
+++ HospitalWeb/HospitalWeb/Middlewares/Implementations/DbInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using HospitalWeb.DAL.Data;
 using HospitalWeb.DAL.Entities.Identity;
+using HospitalWeb.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 
 namespace HospitalWeb.Middlewares.Implementations
@@ -13,45 +14,14 @@ namespace HospitalWeb.Middlewares.Implementations
             _next = next;
         }
 
-        public async Task InvokeAsync(HttpContext context, AppDbContext db, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+        public async Task InvokeAsync(
+            HttpContext context, 
+            AppDbContext db, 
+            IDbInitializer dbInitializer)
         {
             ArgumentNullException.ThrowIfNull(db, nameof(db));
 
-            if (await roleManager.FindByNameAsync("Admin") == null)
-            {
-                await roleManager.CreateAsync(new IdentityRole("Admin"));
-            }
-            if (await roleManager.FindByNameAsync("Patient") == null)
-            {
-                await roleManager.CreateAsync(new IdentityRole("Patient"));
-            }
-            if (await roleManager.FindByNameAsync("Doctor") == null)
-            {
-                await roleManager.CreateAsync(new IdentityRole("Doctor"));
-            }
-
-            if (await userManager.FindByEmailAsync("ihornekhaienko@gmail.com") == null)
-            {
-                string email = "ihornekhaienko@gmail.com";
-                string password = "Pass_1111";
-
-                var admin = new Admin
-                {
-                    Name = "Ihor",
-                    Surname = "Nekhaienko",
-                    UserName = email,
-                    Email = email,
-                    PhoneNumber = "+380500558375",
-                    EmailConfirmed = true,
-                    IsSuperAdmin = true
-                };
-
-                var result = await userManager.CreateAsync(admin, password);
-                if (result.Succeeded)
-                {
-                    await userManager.AddToRoleAsync(admin, "Admin");
-                }
-            }
+            await dbInitializer.Init();
 
             await _next.Invoke(context);
         }
