da23cfdc18a6a46a1df168c6b8a94c0e6968e05a
Igor <ihornekhaienko@gmail.com>
2022-12-11 22:17:09 +0200

    Add Specialty Statistics

--- HospitalWeb/HospitalWeb/Controllers/StatisticsController.cs
+++ HospitalWeb/HospitalWeb/Controllers/StatisticsController.cs
@@ -1,6 +1,5 @@
 ﻿using HospitalWeb.Filters.Builders.Implementations;
 using HospitalWeb.Filters.Models.DTO;
-using HospitalWeb.Filters.Models.ViewModels;
 using HospitalWeb.ViewModels.Statistics;
 using HospitalWeb.WebApi.Clients.Implementations;
 using Microsoft.AspNetCore.Mvc;
@@ -27,7 +26,7 @@ namespace HospitalWeb.Controllers
             var builder = new AppointmentsViewModelBuilder(_api, state: 5, fromTime: fromDate, toTime: toDate, pageSize: int.MaxValue);
             var director = new ViewModelBuilderDirector();
             director.MakeViewModel(builder);
-            var viewModel = builder.GetViewModel();
+            var viewModel = builder.GetViewModel(); 
 
             return View(viewModel);
         }
@@ -39,6 +38,36 @@ namespace HospitalWeb.Controllers
                 .GroupBy(a => a.AppointmentDate.Date)
                 .Select(g => new AttendanceStatViewModel { VisitDate = g.Key, VisitsCount = g.Count() })
                 .ToList();
+
+            return Json(model);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Specialty([FromBody] List<AppointmentDTO> appointments)
+        {
+            var rawModel = appointments
+                .GroupBy(a => a.DoctorSpecialty)
+                .Select(g => new SpecialtyStatViewModel { Label = g.Key, Count = g.Count() })
+                .ToList();
+
+            var model = rawModel
+                .Take(5)
+                .ToList();
+
+            if (rawModel.Count > 5)
+            {
+                var other = new SpecialtyStatViewModel
+                {
+                    Label = "Other",
+                    Count = rawModel
+                        .Skip(5)
+                        .Sum(m => m.Count)
+                };
+
+                model.Add(other);
+            }
 
             return Json(model);
         }
---
+++ HospitalWeb/HospitalWeb/ViewModels/Statistics/SpecialtyStatViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HospitalWeb.ViewModels.Statistics
+{
+    public class SpecialtyStatViewModel
+    {
+        public string Label { get; set; }
+
+        public int Count { get; set; }
+    }
+}
