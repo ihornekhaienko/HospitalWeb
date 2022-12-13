0c282091cf956a05806bb8594676b4a7b45e4b86
Igor <ihornekhaienko@gmail.com>
2022-12-13 15:46:13 +0200

    Create InternalTokenValidator.cs

---
+++ HospitalWeb/HospitalWeb.WebApi/Utility/InternalTokenValidator.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace HospitalWeb.WebApi.Utility
+{
+    public class InternalTokenValidator : ISecurityTokenValidator
+    {
+        private readonly JwtSecurityTokenHandler _tokenHandler;
+
+        public InternalTokenValidator()
+        {
+            _tokenHandler = new JwtSecurityTokenHandler();
+        }
+
+        public bool CanValidateToken => true;
+
+        public int MaximumTokenSizeInBytes { get; set; } = TokenValidationParameters.DefaultMaximumTokenSizeInBytes;
+
+        public bool CanReadToken(string securityToken)
+        {
+            return _tokenHandler.CanReadToken(securityToken);
+        }
+
+        public ClaimsPrincipal ValidateToken(string securityToken, TokenValidationParameters validationParameters, out SecurityToken validatedToken)
+        {
+            validatedToken = null;
+            try
+            {
+                var principal = _tokenHandler.ValidateToken(securityToken, validationParameters, out validatedToken);
+
+                return principal;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
