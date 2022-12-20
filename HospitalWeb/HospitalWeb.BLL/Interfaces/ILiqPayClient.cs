852c7d722af04204fbbabd417eb82d0ef375c72a
Igor <ihornekhaienko@gmail.com>
2022-12-20 17:11:29 +0200

    Create ILiqPayClient.cs

---
+++ HospitalWeb/HospitalWeb.BLL/Interfaces/ILiqPayClient.cs
@@ -0,0 +1,9 @@
+﻿using HospitalWeb.Services.Utility;
+
+namespace HospitalWeb.Services.Interfaces
+{
+    public interface ILiqPayClient
+    {
+        public Task<LiqPayResponse> RequestAsync(string path, LiqPayRequest request);
+    }
+}
