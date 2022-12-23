485d6795d4b2f0bfa80d9b46cb44d931276e9d5e
Igor <ihornekhaienko@gmail.com>
2022-12-23 14:20:07 +0200

    Update PatientsController.cs

--- HospitalWeb/HospitalWeb.WebApi/Controllers/PatientsController.cs
+++ HospitalWeb/HospitalWeb.WebApi/Controllers/PatientsController.cs
@@ -200,8 +200,6 @@ namespace HospitalWeb.WebApi.Controllers
                     return BadRequest("Passing null object to the PatientsController.Post method");
                 }
 
-                var headers = Request.Headers;
-
                 var config = new MapperConfiguration(cfg => cfg.CreateMap<PatientResourceModel, Patient>()
                     .ForMember(d => d.Image, o => o.Ignore()));
                 var mapper = new Mapper(config);
