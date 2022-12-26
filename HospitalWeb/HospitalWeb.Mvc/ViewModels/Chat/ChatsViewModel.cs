d97a775f60c449e92fc030fe3654df2fdd7baea2
Igor <ihornekhaienko@gmail.com>
2022-12-26 21:13:44 +0200

    Add ViewModels

---
+++ HospitalWeb/HospitalWeb.Mvc/ViewModels/Chat/ChatsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HospitalWeb.Mvc.ViewModels.Chat
+{
+    public class ChatsViewModel
+    {
+        public string UserId { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime LastMessageDateTime { get; set; }   
+
+        public string LastMessage { get; set; }
+    }
+}
---
+++ HospitalWeb/HospitalWeb.Mvc/ViewModels/Chat/MessageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using HospitalWeb.Domain.Entities;
+
+namespace HospitalWeb.Mvc.ViewModels.Chat
+{
+    public class MessageViewModel
+    {
+        public string Text { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public MessageType MessageType { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
