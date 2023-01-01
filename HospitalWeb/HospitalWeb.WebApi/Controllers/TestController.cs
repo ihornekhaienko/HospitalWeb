62a8a68798110f7c21c7f473d584410209d7ec91
Igor <ihornekhaienko@gmail.com>
2023-01-01 20:10:37 +0200

    Create TestController.cs

---
+++ HospitalWeb/HospitalWeb.WebApi/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using HospitalWeb.WebApi.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalWeb.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly IDbInitializer _dbInitializer;
+
+        public TestController(IDbInitializer dbInitializer)
+        {
+            _dbInitializer = dbInitializer;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await _dbInitializer.GenerateDb();
+
+            return Ok();
+        }
+    }
+}
