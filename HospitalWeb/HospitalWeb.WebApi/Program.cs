747ddba444b0ef9c9ca581d8899a2f95524f66b0
Igor <ihornekhaienko@gmail.com>
2023-01-02 17:34:53 +0200

    Update Program.cs

--- HospitalWeb/HospitalWeb.WebApi/Program.cs
+++ HospitalWeb/HospitalWeb.WebApi/Program.cs
@@ -191,12 +191,8 @@ builder.Services.AddDbInitializer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseRequestLocalization();
 
